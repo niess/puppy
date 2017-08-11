27e48e4aa67841d455613250c7a85261b088064e
Valentin Niess <niess@in2p3.fr>
2017-08-11 14:11:55 +0200

    Add a texture to the box faces and optional border lines.

--- test-cube.py
+++ test-cube.py
@@ -2,19 +2,30 @@ from direct.showbase.ShowBase import ShowBase
 from panda3d.core import *
 
 class Box:
-    def __init__(self, x, y, z, color):
+    def __init__(self, dx, dy, dz, face_color=(1,1,1,1), line_color=(0,0,0,1),
+      texture=None):
         # Build the data vector for the faces.
-        format = GeomVertexFormat.getV3c4()
+        format = GeomVertexFormat.getV3c4t2()
         data = GeomVertexData("vertices", format, Geom.UHStatic)
-        data.setNumRows(8)
+        data.setNumRows(24)
         writer = GeomVertexWriter(data, "vertex")
-        for i in xrange(2):
-            for j in xrange(2):
-                for k in xrange(2):
-                    writer.addData3f(
-                      (i - 0.5) * x, (j - 0.5) * y, (k - 0.5) * z)
+        for axis in ((0, 1, 2), (1, 2, 0), (2, 0, 1)):
+            for c in ((0, 0, 0), (0, 1, 0), (1, 1, 0), (1, 0, 0)):
+                writer.addData3f((c[axis[0]] - 0.5) * dx,
+                                 (c[axis[1]] - 0.5) * dy,
+                                 (c[axis[2]] - 0.5) * dz)
+            for c in ((0, 0, 1), (1, 0, 1), (1, 1, 1), (0, 1, 1)):
+                writer.addData3f((c[axis[0]] - 0.5) * dx,
+                                 (c[axis[1]] - 0.5) * dy,
+                                 (c[axis[2]] - 0.5) * dz)
         writer = GeomVertexWriter(data, "color")
-        for _ in xrange(8): writer.addData4f(color)
+        for _ in xrange(24): writer.addData4f(face_color)
+        writer = GeomVertexWriter(data, "texcoord")
+        for _ in xrange(6):
+            writer.addData2f(0, 0)
+            writer.addData2f(1, 0)
+            writer.addData2f(1, 1)
+            writer.addData2f(0, 1)
 
         def connect(object, indices):
             """Connect the vertices of a GeomPrimitive
@@ -25,11 +36,9 @@ class Box:
 
         # Build the triangles.
         triangles = GeomTriangles(Geom.UHStatic)
-        for i, j, k in ((4,1,2),(2,4,1),(1,2,4)):
-            connect(triangles, (0, j, k))
-            connect(triangles, (j + k, k, j))
-            connect(triangles, (i, i + k, i + j))
-            connect(triangles, (i + j + k, i + j, i + k))
+        for i in xrange(6):
+            connect(triangles, (4 * i + 1, 4 * i + 2, 4 * i))
+            connect(triangles, (4 * i + 2, 4 * i + 3, 4 * i))
 
         # Build the Geom for the faces and initialise the node.
         self.faces = Geom(data)
@@ -37,48 +46,52 @@ class Box:
         node = GeomNode("box")
         node.addGeom(self.faces)
 
-        # Build the data vector for the borders.
-        format = GeomVertexFormat.getV3c4()
-        data = GeomVertexData("vertices", format, Geom.UHStatic)
-        data.setNumRows(8)
-        writer = GeomVertexWriter(data, "vertex")
-        for i in xrange(2):
-            for j in xrange(2):
-                for k in xrange(2):
-                    writer.addData3f(
-                      (i - 0.5) * x, (j - 0.5) * y, (k - 0.5) * z)
-        writer = GeomVertexWriter(data, "color")
-        for _ in xrange(8): writer.addData4f((0,0,0,1))
+        # Build the data vector for the border lines.
+        if line_color is not None:
+            format = GeomVertexFormat.getV3c4()
+            data = GeomVertexData("vertices", format, Geom.UHStatic)
+            data.setNumRows(8)
+            writer = GeomVertexWriter(data, "vertex")
+            for i in xrange(2):
+                for j in xrange(2):
+                    for k in xrange(2):
+                        writer.addData3f(
+                          (i - 0.5) * dx, (j - 0.5) * dy, (k - 0.5) * dz)
+            writer = GeomVertexWriter(data, "color")
+            for _ in xrange(8): writer.addData4f(line_color)
 
-        # Build the skeleton.
-        lines = GeomLines(Geom.UHStatic)
-        connect(lines, (0,1))
-        connect(lines, (0,2))
-        connect(lines, (0,4))
-        connect(lines, (3,1))
-        connect(lines, (3,2))
-        connect(lines, (3,7))
-        connect(lines, (5,1))
-        connect(lines, (5,4))
-        connect(lines, (5,7))
-        connect(lines, (6,2))
-        connect(lines, (6,4))
-        connect(lines, (6,7))
+            # Build the skeleton.
+            lines = GeomLines(Geom.UHStatic)
+            connect(lines, (0,1))
+            connect(lines, (0,2))
+            connect(lines, (0,4))
+            connect(lines, (3,1))
+            connect(lines, (3,2))
+            connect(lines, (3,7))
+            connect(lines, (5,1))
+            connect(lines, (5,4))
+            connect(lines, (5,7))
+            connect(lines, (6,2))
+            connect(lines, (6,4))
+            connect(lines, (6,7))
 
-        # Build the Geom for the borders and add it to the node.
-        self.borders = Geom(data)
-        self.borders.addPrimitive(lines)
-        node.addGeom(self.borders)
+            # Build the Geom for the borders and add it to the node.
+            self.borders = Geom(data)
+            self.borders.addPrimitive(lines)
+            node.addGeom(self.borders)
 
         # Render the node.
         self.node = render.attachNewNode(node)
+        if texture is not None:
+            self.node.setTexture(texture)
 
 class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
         # Create a box.
-        self.box = Box(10., 10., 10., (1., 1., 1., 1.))
+        texture = loader.loadTexture("maps/envir-reeds.png")
+        self.box = Box(10., 10., 10., texture=texture)
 
 app = MyApp()
 app.run()
